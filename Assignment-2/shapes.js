e8b45eff8dbaa09b5c3a400d699e32f9d1f3caef
tragen-va <tragenva@gmail.com>
2024-09-30 10:47:24 -0700

    Create shapes.js

---
+++ Assignment-2/shapes.js
@@ -0,0 +1,15 @@
+let gl = undefined;
+
+function init() {
+    let canvas = document.getElementById("webgl-canvas");
+    gl = canvas.getContext("webgl2");
+    if (!gl) { alert("Your Web browser doesn't support WebGL 2\nPlease contact Dave"); }
+
+    // Add initialization code here
+}
+
+function render() {
+    // Add rendering code here
+}
+
+window.onload = init;
