b5f984784cc5d48bd233abba9c2ece75ad14b679
Tragen <tragen>
2024-12-07 22:21:09 -0800

    added test import of mountians gltf model

--- Final-Project/main.js
+++ Final-Project/main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -10,37 +11,44 @@ document.body.appendChild( renderer.domElement );
 
 
 
-
-// box geometry
-const boxGeometry = new THREE.BoxGeometry( 1, 1, 1);
-const boxMaterial = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( boxGeometry, boxMaterial );
-scene.add( cube );
+/////// load gltfl ///////////
 
 
+const loader = new GLTFLoader();
 
-// plane
+loader.load( 'models/scene.gltf', function ( gltf ) {
 
-const planeGeometry = new THREE.PlaneGeometry( 5, 50, 10, 10); 
-//const planeMaterial = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} ); 
-const planeMaterial = new THREE.MeshPhongMaterial( {color: 0xffff00, side: THREE.DoubleSide} ); 
-const plane = new THREE.Mesh( planeGeometry, planeMaterial ); 
-scene.add( plane );
-plane.rotation.x -= 0.9;
+	scene.add( gltf.scene );
+
+}, undefined, function ( error ) {
+
+	console.error( error );
+
+} );
+
+
+
+
+
+
+
+
+
+
 
 // light
-
-const light = new THREE.DirectionalLight(0xffffff, 1)
-light.position.set(0, 0, 1)
-scene.add(light)
-
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(0, 0, 1);
+scene.add(light);
 
 
-camera.position.z = 5;
+
+camera.position.z = 0;
+camera.position.y = 5;
+camera.position.x = -12;
+
 function animate() {
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
 	renderer.render( scene, camera );
 }
 renderer.setAnimationLoop( animate );
